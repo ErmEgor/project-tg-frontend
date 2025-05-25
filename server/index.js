16dc236934e5fa6cff792b53010d9ce347ebe4e3
Егор <ermilegor@gmail.com>
2025-05-25 10:23:59 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { Telegraf } = require('telegraf');
 const app = express();
-const bot = new Telegraf('7650306469:AAHoO8xxuJwFV8fvJ6tyclzl6cuYYSqJYM8'); // Замени на токен от @BotFather
+const bot = new Telegraf('7711881075:AAHIzBzogyLUSeitdeWzR2Ahq-KpN1MTR9U'); // Замени на токен от @BotFather
 
 app.use(express.static('../telegram-mini-app/build'));
 
@@ -9,7 +9,7 @@ bot.command('start', (ctx) => {
   ctx.reply('Нажми кнопку ниже!', {
     reply_markup: {
       inline_keyboard: [
-        [{ text: 'Открыть приложение', web_app: { url: 'https://6c7c-95-182-105-163.ngrok-free.app' } }]
+        [{ text: 'Открыть приложение', web_app: { url: 'https://telegram-mini-9vtyr1wee-ermegors-projects.vercel.app/' } }]
       ]
     }
   });
